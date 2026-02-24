014a06b7c373bb42a81aad0adc680e7da51d5af9
Jason <qibeier.mou@itas.ca>
2026-02-23 18:38:55 -0800

    Other fix on coverage

--- daily-time-tracker/src/utils/timeUtils.test.js
+++ daily-time-tracker/src/utils/timeUtils.test.js
@@ -1,25 +1,23 @@
-import { addTimes,subtract,secondsToMinutes,formatTime,reset } from "./timeUtils";
+import { addTimes, subtract, secondsToMinutes, formatTime, reset } from "./timeUtils";
 
 describe("Time Utility Functions", () => {
-
-  it("adds two numbers", () => {
-    expect(addTimes(5, 5)).toBe(10);
+  test("addTimes adds numbers", () => {
+    expect(addTimes(2, 3)).toBe(5);
   });
 
-  it("subtracts two numbers", () => {
-    expect(subtract(10, 5)).toBe(5);
+  test("subtract subtracts numbers", () => {
+    expect(subtract(5, 3)).toBe(2);
   });
 
-  it("converts seconds to minutes", () => {
+  test("secondsToMinutes converts correctly", () => {
     expect(secondsToMinutes(120)).toBe(2);
   });
 
-  it("formats seconds into minutes and seconds", () => {
-    expect(formatTime(125)).toBe("2:5");
+  test("formatTime returns minutes:seconds", () => {
+    expect(formatTime(125)).toBe("2:05"); 
   });
 
-  it("resets value to 0", () => {
+  test("reset returns 0", () => {
     expect(reset()).toBe(0);
   });
-
 });
