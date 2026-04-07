241ca0cd66b1e5dfd7057b81b7e9aee98d9e37e8
Jason <qibeier.mou@itas.ca>
2026-04-06 19:57:02 -0700

    fix ui test and run

--- DailyTimeTracker/tests/ui/edit.test.jsx
+++ DailyTimeTracker/tests/ui/edit.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { describe, it, expect } from '@jest/globals';
 import { render } from '@testing-library/react-native';
 import EditScreen from '../../app/(tabs)/edit';
 // Mock router if used
